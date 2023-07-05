0207f32f7142a0b68616ad302f19f3b38748f0a7
Cano-Jones <alexcanojones@gmail.com>
2023-07-05 13:20:34 +0200

    First collision mechanism implemented

--- Physics_Engine.py
+++ Physics_Engine.py
@@ -5,6 +5,9 @@ import pygame
 import sys
 import time
 from random import uniform
+from math import sqrt
+from itertools import combinations
+
 
 
 
@@ -13,12 +16,14 @@ Width=500
 Height=500
 window_size=(Width,Height)
 World='Closed'
-
 screen=pygame.display.set_mode(window_size)
 pygame.display.set_caption('2D Physics Engine')
 
+def Distance(p1, p2):
+    return sqrt((p1.Position[0]-p2.Position[0])**2+(p1.Position[1]-p2.Position[1])**2)
+
 def Force(p):
-    return [0,800]
+    return [0,2000]
 
 class Particle():
     def __init__(self, position, velocity, mass, radius):
@@ -72,7 +77,7 @@ class Particle():
         
         
 
-def Draw_Window(t):
+def Draw_Window():
     
     screen.fill('white')
     for p in System:
@@ -80,19 +85,34 @@ def Draw_Window(t):
     
     pygame.display.update()
 
-t=0
-System=[Particle([uniform(0,250),uniform(0,250)],[uniform(-50,50),uniform(-10,10)],10,10) for i in range(5)]
-while True:
-    start=time.process_time()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+def Colision(p1,p2):
+    n=[p1.Position[d]-p2.Position[d] for d in range(2)]
+    aux=0
+    for d in range(2): aux+=n[d]*(p2.Velocity[d]-p1.Velocity[d])
+    c=2.0*aux/((n[0]**2+n[1]**2)*(1.0/p1.M+1.0/p2.M))
+    
+    p1.Velocity=[p1.Velocity[d]+c/p1.M*n[d] for d in range(2)]
+    p2.Velocity=[p2.Velocity[d]-c/p2.M*n[d] for d in range(2)]
+
+def Next_Step():
     for p in System:
         p.Move()
-    Draw_Window(t)
     
-    
-    t+=dt
-    finish=time.process_time()
-    if (finish-start<dt): time.sleep(dt-(finish-start))
+    for comb in combinations(System, 2):
+        if Distance(comb[0], comb[1])<(comb[0].R+comb[1].R):
+            Colision(comb[0],comb[1])
+    Draw_Window()
+
+
+
+if __name__ == "__main__":
+    System=[Particle([uniform(0,250),uniform(0,250)],[uniform(-50,50),uniform(-10,10)],10,10) for i in range(20)]
+    while True:
+        start=time.process_time()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        Next_Step()
+        finish=time.process_time()
+        if (finish-start<dt): time.sleep(dt-(finish-start))
     
