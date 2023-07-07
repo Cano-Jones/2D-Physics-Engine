a4803f96861779f4593c622e81d0ace9c6ec46e0
Cano-Jones <alexcanojones@gmail.com>
2023-07-07 19:02:49 +0200

    First Wall colision implementation

--- Physics_Engine.py
+++ Physics_Engine.py
@@ -15,13 +15,13 @@ dt=0.01
 Width=600
 Height=600
 window_size=(Width,Height)
-World='Circle'
+World='Closed'
 Force_Type='FreeFall'
 screen=pygame.display.set_mode(window_size)
 pygame.display.set_caption('2D Physics Engine')
 clock = pygame.time.Clock()
 
-def Distance(p1, p2):
+def Particle_Particle_Distance(p1, p2):
     if World=='Periodic':
         #Distances are different here
         pass
@@ -33,15 +33,35 @@ def Force(p):
     if Force_Type=='FreeFall': return [0,500*p.M]
     if Force_Type=='Coulomb':
         # If World== Periodic, this has to change
-        G=50
+        G=100000
         F=[0,0]
-        for q in System:
+        for q in Particle_System:
             if q is not p:
                 cte=G*p.M*q.M
-                R=Distance(p,q)
-                F[0]-=cte/R/R*(p.Position[0]-q.Position[0])
-                F[1]-=cte/R/R*(p.Position[1]-q.Position[1])
+                R=Particle_Particle_Distance(p,q)
+                F[0]-=cte/R/R/R*(p.Position[0]-q.Position[0])
+                F[1]-=cte/R/R/R*(p.Position[1]-q.Position[1])
         return F
+
+def Particle_Line_Distance(p,l):
+    if p.Position[0]<max(l.A[0],l.B[0]) and p.Position[0]>min(l.A[0],l.B[0]):
+        if p.Position[1]<max(l.A[1],l.B[1]) and p.Position[1]>min(l.A[1],l.B[1]):
+        
+            aux1=(l.B[0]-l.A[0])*(l.A[1]-p.Position[1])
+            aux2=(l.A[0]-p.Position[0])*(l.B[1]-l.A[1])
+            aux3=(l.B[0]-l.A[0])**2+(l.B[1]-l.A[1])**2
+            D=abs(aux1-aux2)/sqrt(aux3)
+            return D
+    return max(Width,Height)
+    
+    
+class Line():
+    def __init__(self, A, B, color):
+        self.A=A
+        self.B=B
+        self.Color=color
+        self.Angle=atan2((A[1]-B[1]),(A[0]-B[0]))
+        
 
 class Particle():
     def __init__(self, position, velocity, mass=10, radius=10, color='White'):
@@ -117,9 +137,13 @@ def Draw_Window():
         screen.fill('gray')
         pygame.draw.circle(screen, 'white', [Width/2,Height/2], min(Height,Width)/2)
         pygame.draw.circle(screen, 'black', [Width/2,Height/2], min(Height,Width)/2, width=2)
-        
-    for p in System:
+    
+    for l in Line_System:
+        pygame.draw.line(screen, l.Color, l.A, l.B, width=5)
+         
+    for p in Particle_System:
         pygame.draw.circle(screen, p.Color, p.Position, p.R)
+    
     
     pygame.display.update()
     
@@ -130,7 +154,7 @@ def Particle_Particle_Colision(p1,p2):
     
     
     aux=[p1.Position[d]-p2.Position[d] for d in range(2)]
-    norm=Distance(Particle(aux,aux),Particle([0,0],aux))
+    norm=Particle_Particle_Distance(Particle(aux,aux),Particle([0,0],aux))
     if norm!=0:
         n_norm=[aux[d]/norm for d in range(2)]
         Delta=(p1.R+p2.R-norm)
@@ -140,7 +164,7 @@ def Particle_Particle_Colision(p1,p2):
     
     
     n=[p1.Position[d]-p2.Position[d] for d in range(2)]
-    norm=Distance(Particle(n,n),Particle([0,0],n))
+    norm=Particle_Particle_Distance(Particle(n,n),Particle([0,0],n))
     if norm!=0:
         aux=0
         for d in range(2): aux+=n[d]*(p2.Velocity[d]-p1.Velocity[d])
@@ -149,13 +173,27 @@ def Particle_Particle_Colision(p1,p2):
         p1.Velocity=[p1.Velocity[d]+c/p1.M*n[d] for d in range(2)]
         p2.Velocity=[p2.Velocity[d]-c/p2.M*n[d] for d in range(2)]
 
+def Particle_Line_Colision(p,l):
+    Angle=l.Angle
+    Vx,Vy=p.Velocity
+    aux_x=Vx
+    aux_y=Vy
+    Vx=cos(2*Angle)*aux_x+aux_y*sin(2*Angle)
+    Vy=sin(2*Angle)*aux_x-aux_y*cos(2*Angle)
+    p.Velocity=[Vx,Vy]
+
 def Next_Step():
-    for p in System:
+    for p in Particle_System:
         p.Move()
     
-    for comb in combinations(System, 2):
-        if Distance(comb[0], comb[1])<(comb[0].R+comb[1].R):
+    for comb in combinations(Particle_System, 2):
+        if Particle_Particle_Distance(comb[0], comb[1])<(comb[0].R+comb[1].R):
             Particle_Particle_Colision(comb[0],comb[1])
+    
+    for p in Particle_System:
+        for l in Line_System:
+            if Particle_Line_Distance(p,l)<p.R:
+                Particle_Line_Colision(p,l)
     
     
     Draw_Window()
@@ -163,11 +201,13 @@ def Next_Step():
 
 
 if __name__ == "__main__":
-    System=[]
-    #System=[Particle([uniform(Width*1/3,Width*2/3),uniform(Height/3,Height*2/3)],[uniform(-10,10),uniform(-10,10)],2,3,'skyblue3') for i in range(200)]
-    System.append(Particle([Width*2/3, Height/2], [0,0], 30, 30, 'red'))
-    System.append(Particle([Width/2,Height/2], [0,0], 20, 20, 'green'))
-    System.append(Particle([Width/3,Height/2], [0,0], 15, 15, 'blue'))
+    Particle_System=[]
+    Line_System=[]
+    Line_System.append(Line([0,Height],[Width/2,Height/2], 'black'))
+    #Particle_System=[Particle([uniform(Width*1/3,Width*2/3),uniform(Height/3,Height*2/3)],[uniform(-10,10),uniform(-10,10)],2,3,'skyblue3') for i in range(200)]
+    Particle_System.append(Particle([Width*2/3, Height/2], [0,0], 30, 30, 'red'))
+    Particle_System.append(Particle([Width*2/5,Height*2/3], [0,500], 20, 20, 'green'))
+    Particle_System.append(Particle([Width/3,Height/2], [0,0], 15, 15, 'blue'))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
