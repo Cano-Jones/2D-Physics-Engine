3e79c482fdf01e56091e38631b48b939b2688dca
Cano-Jones <alexcanojones@gmail.com>
2023-07-14 11:38:15 +0200

    Example scripts updated

--- Example_Scripts/Circle_Boundary.py
+++ Example_Scripts/Circle_Boundary.py
@@ -4,4 +4,4 @@ particle_1=Particle(Position=[200,100], Velocity=[100,0], Mass=30, Radius=30, Co
 particle_2=Particle(Position=[400,100], Velocity=[20,0], Mass=20, Radius=20, Color='blue')
 particle_3=Particle(Position=[300,300], Velocity=[0,0], Mass=15, Radius=35, Color='green')
 Particle_System=[particle_1, particle_2, particle_3]
-Engine(Particle_System=Particle_System, Force = lambda x: [0,500*x.Mass], Boundary=Closed_Circle_Boundary, Background=Circle_Boundary_Background)
+Engine(Particle_System=Particle_System, Force = Const_Gravity(200), Boundary=Closed_Circle_Boundary, Background=Circle_Boundary_Background)
--- Example_Scripts/Pachinko.py
+++ Example_Scripts/Pachinko.py
@@ -15,4 +15,4 @@ line_8=Line(Point_A=[400,300], Point_B=[350,350])
 Particle_System=[Particle(Position=[ran(200,400), ran(75,100)], Color=Random_Color()) for n in range(25)]
 Line_System=[line_1, line_2, line_3, line_4, line_5, line_6, line_7, line_8]
 
-Engine(Window_Size=Window, Particle_System=Particle_System, Line_System=Line_System, Force = lambda x: [0,100*x.Mass])
+Engine(Window_Size=Window, Particle_System=Particle_System, Line_System=Line_System, Force = Const_Gravity(100))
